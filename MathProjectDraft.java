b10ac9732be55085e9852a0dbf461b42457d4031
townseda27 <82985709+townseda27@users.noreply.github.com>
2021-04-24 10:04:38 -0400

    Add files via upload
    Everything is functional. Could probably be cleaned up a little. Specifically, the formatting of the printed subsets could be improved.

--- MathProjectDraft.java
+++ MathProjectDraft.java
@@ -1,19 +1,13 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
-
 /**
- * Code for the math project (MTH 231). Not done yet. 
+ * MTH 231 Group Project code. Produces a list of all distinct subsets
+ * of size k from the set {1, 2, 3,..., n}. Also, using the definition
+ * of n choose k, this program calculates the value of n choose k.
  * 
- * 
- * 
- * TODO figure out how to find the list of possible subsets. everything else is done.
- * 
- * 
- * 
- * @author Daniel Townsend
- *
+ * Group Members: Anthoney Brey, Colin McIntyre, Daniel Townsend, Dalong Wang,
+ *                Elton Zeng, Isabella Wilson & Yuming Deng.
  */
 public class MathProjectDraft {
 
@@ -21,26 +15,30 @@ public class MathProjectDraft {
 		
 		Scanner inputReader = new Scanner(System.in);
 		
+		// get n (number of elements to choose from)
 		System.out.print("Enter a value for n: ");
 		int numOfElements = inputReader.nextInt(); // n
 		
+		// get k (size of subsets)
 		System.out.print("Enter a value for k: ");
 		int sizeOfSubsets = inputReader.nextInt(); // k
 		inputReader.close();
 		
+		// print how many possible subsets there are given n and k
 		System.out.println(numOfElements + " choose " + sizeOfSubsets + " is "
 				           + countPossibleSubsets(numOfElements, sizeOfSubsets));
 		
+		// print and generate the possible subsets given n and k
 		ArrayList<ArrayList<Integer>> listOfSubsets = new ArrayList<ArrayList<Integer>>();
-
+		getPossibleSubsets(numOfElements, sizeOfSubsets, new ArrayList<Integer>(), listOfSubsets, 1);
 		printSubsets(listOfSubsets);
 	}
 	
 	/**
-	 * Finds the factorial of an integer.
+	 * Recursively finds the factorial of an integer.
 	 * 
 	 * @param n the integer to find the factorial of.
-	 * @return an integer holding the factorial of n.
+	 * @return the factorial of n as an integer.
 	 */
 	public static int factorial(int n) {
 		if (n == 1) {
@@ -51,44 +49,79 @@ public class MathProjectDraft {
 	}
 	
 	/**
-	 * Counts the possible subsets of n choose k using the given formula.
+	 * Counts the number of possible subsets of n choose k 
+	 * using the given formula.
 	 * 
-	 * @param n the number of elements in the "main" set.
+	 * @param n the number of elements to choose from to create
+	 * subsets.
 	 * @param k the number of elements in the subsets.
-	 * @return an integer which is the result of the n choose k formula.
+	 * @return the result of the n choose k formula as an integer.
 	 */
 	public static int countPossibleSubsets(int n, int k) {
-		if(k == 0 || n < k) {
-			return 0;
-		}
 		return (factorial(n)) / (factorial(k) * factorial(n - k));
-	}
-
-	// WORK IN PROGRESS
-	private static void getPossibleSubsets(int n, int k, ArrayList<ArrayList<Integer>> listOfSubsets) {
-		
-		
-		// probably need to use recursion here
-		
-		
-		
 	}
 	
 	/**
-	 * Prints all the ArrayLists within an ArrayList formatted nicely.
+	 * Recursively generates all the possible subsets based on the n and k
+	 * values.
 	 * 
-	 * @param listOfSubsets an ArrayList of Integer ArrayLists that holds all the
-	 * possible subsets given n and k.
+	 * @param n the number of elements to choose from to create
+	 * subsets.
+	 * @param k the number of elements in the subsets.
+	 * @param subset an ArrayList of Integers which represents the current
+	 *        subset.
+	 * @param listOfSubsets an ArrayList of ArrayLists of Integers which holds
+	 *        all the possible subsets.
+	 * @param start the starting value to add to the subset. Initially, the
+	 *        value 1 is passed as the argument.
+	 */
+	public static void getPossibleSubsets(int n, int k, ArrayList<Integer> subset,  ArrayList<ArrayList<Integer>> listOfSubsets, int start) {
+		
+		// the base case. when building up the subsets, we know a subset is
+		// "complete" if its size is equal to k.
+		if(subset.size() == k) {
+			listOfSubsets.add(subset);
+			return;
+		} 
+		
+		/*
+		 * This for loop loops through all the integers 1 through n
+		 * inclusive. Each time, we add the index to the current subset and
+		 * call the method again but with the starting index equal to the
+		 * previous index plus one. 
+		 */
+		for(int i = start; i <= n; i++) {
+			/*
+			 * We call the method again on a copy of the subset because ArrayLists are
+			 * passed by reference, so we don't want to continually edit the same
+			 * subset/ArrayList.
+			 */
+			ArrayList<Integer> copySet = new ArrayList<Integer>(subset);
+			copySet.add(i);
+			getPossibleSubsets(n, k, copySet, listOfSubsets, i + 1);
+		}
+	}
+	  
+	/**
+	 * Prints all the ArrayLists within an ArrayList formatted as such:
+	 * 1. {1, 2} 
+	 * 2. {1, 3}
+	 * 3. {2, 3}
+	 * 
+	 * @param listOfSubsets an ArrayList of Integer ArrayLists which holds all the
+	 * possible subsets made with the given n and k values.
 	 */
 	public static void printSubsets(ArrayList<ArrayList<Integer>> listOfSubsets) {
 		System.out.println();
 		for(int i = 0; i < listOfSubsets.size(); i++) {
+			
 			System.out.print(i + 1 + ". {");
 			for(int j = 0; j < listOfSubsets.get(i).size(); j++) {
 				
 				if(j != listOfSubsets.get(i).size() - 1) {
 					System.out.print(listOfSubsets.get(i).get(j) + ", ");
 				} else {
+					// if we reach the end of a subset, we use a "}"
 					System.out.println(listOfSubsets.get(i).get(j) + "}");
 				}				
 			}
